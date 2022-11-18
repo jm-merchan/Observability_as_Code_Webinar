4bea78122acc7d0fbf4f0d9a508020c15fd80264
jm-merchan <jose.merchan@hashicorp.com>
2022-11-18 15:24:27 +0100

    removing defaults

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  #default     = "us-east-1"
 }
 
 variable "vpc_name" {
   type    = string
-  default = "datadog-webminar"
+  #default = "datadog-webminar"
 
 }
