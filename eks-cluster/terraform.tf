c53b91d64062da2748a4c307da08f4775c45028c
jm-merchan <jose.merchan@hashicorp.com>
2022-11-18 15:31:50 +0100

    changes in providers versions

--- eks-cluster/terraform.tf
+++ eks-cluster/terraform.tf
@@ -2,30 +2,30 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 4.15.0"
+      version = "~> 4.39.0"
     }
 
     random = {
       source = "hashicorp/random"
-      #version = "~> 3.1.0"
+      version = "~> 3.4.3"
     }
 
     tls = {
       source = "hashicorp/tls"
-      #version = "~> 3.4.0"
+      version = "~> 3.4.0"
     }
 
     cloudinit = {
       source = "hashicorp/cloudinit"
-      #version = "~> 2.2.0"
+      version = "~> 2.2.0"
     }
 
     kubernetes = {
       source = "hashicorp/kubernetes"
-      #version = "~> 2.12.1"
+      version = "~> 2.15.0"
     }
   }
 
-  #required_version = "~> 1.3"
+  required_version = "~> 1.3"
 }
 
