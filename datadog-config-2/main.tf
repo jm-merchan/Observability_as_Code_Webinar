fce1634d6f741fa9403cc1687d100834b370f897
jm-merchan <jose.merchan@hashicorp.com>
2022-11-18 16:30:40 +0100

    variables

--- datadog-config-2/main.tf
+++ datadog-config-2/main.tf
@@ -9,4 +9,7 @@ terraform {
   required_version = "~> 0.13"
 }
 
-provider "datadog" {}
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
--- datadog-config-2/variables.tf
+++ datadog-config-2/variables.tf
@@ -1,5 +1,15 @@
 variable "application" {
   type        = string
   description = "Name of application"
-  default = "eCommerce"
+  # default = "eCommerce"
+}
+
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  type        = string
+  description = "Datadog Application Key"
 }
