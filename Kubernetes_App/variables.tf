75b2ba5530d6b9a617ddf9fd6fa8dc36d000a3d6
jm-merchan <jose.merchan@hashicorp.com>
2022-11-28 10:29:30 +0100

    adding variables.tf

---
+++ Kubernetes_App/variables.tf
@@ -0,0 +1,25 @@
+variable "application_name" {
+  type        = string
+  description = "Application Name"
+  #default     = "beacon"
+}
+variable "aws_region" {
+  type = string
+  #default = "us-east-1"
+}
+
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  type        = string
+  description = "Datadog Application Key"
+}
+
+variable "org_name" {
+  type = string
+}
+
+
